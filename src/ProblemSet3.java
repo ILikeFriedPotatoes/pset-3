09f72b76e84a7c5e046b34e5155df0cf26e93b47
Joseph Wang <jwang@ucvts.org>
2019-10-12 23:24:54 -0400

    Finished exercises 6 and 7

--- src/ProblemSet3.java
+++ src/ProblemSet3.java
@@ -32,15 +32,10 @@ public class ProblemSet3 {
         //ps.parity();        // executes Exercise 2
         //ps.ordered();       // executes Exercise 3
         //ps.gpa();           // executes Exercise 4
-<<<<<<< HEAD
-        ps.grade();         // executes Exercise 5
-        //ps.cards();         // executes Exercise 6
-=======
         //ps.grade();         // executes Exercise 5
-        ps.cards();         // executes Exercise 6
->>>>>>> 387ef04bbb211525fbda129fc75c147fd944d522
+        //ps.cards();         // executes Exercise 6
         //ps.leapYear();      // executes Exercise 7
-        //ps.state();         // executes Exercise 8
+        ps.state();         // executes Exercise 8
         //ps.months();        // executes Exercise 9
         //ps.salary();        // executes Exercise 10
 
@@ -124,30 +119,44 @@ public class ProblemSet3 {
     public void gpa() {
         System.out.print("Enter a letter grade: ");
         String letterGrade = in.nextLine();
-        if (letterGrade.equalsIgnoreCase("A+") || letterGrade.equalsIgnoreCase("A")) {
-            System.out.print("\nYour GPA is 4.00.\n");
-        } else if (letterGrade.equalsIgnoreCase("A-")) {
-            System.out.print("\nYour GPA is 3.66.\n");
-        } else if (letterGrade.equalsIgnoreCase("B+")) {
-            System.out.print("\nYour GPA is 3.33.\n");
-        } else if(letterGrade.equalsIgnoreCase("B")) {
-            System.out.print("\nYour GPA is 3.00.\n");
-        } else if (letterGrade.equalsIgnoreCase("B-")) {
-            System.out.print("\nYour GPA is 2.66.\n");
-        } else if (letterGrade.equalsIgnoreCase("C+")) {
-            System.out.print("\nYour GPA is 2.33.\n");
-        } else if (letterGrade.equalsIgnoreCase("C")) {
-            System.out.print("\nYour GPA is 2.00.\n");
-        } else if (letterGrade.equalsIgnoreCase("C-")) {
-            System.out.print("\nYour GPA is 1.66.\n");
-        } else if (letterGrade.equalsIgnoreCase("D+")) {
-            System.out.print("\nYour GPA is 1.33.\n");
-        } else if (letterGrade.equalsIgnoreCase("D")) {
-            System.out.print("\nYour GPA is 1.00.\n");
-        } else if (letterGrade.equalsIgnoreCase("D-")) {
-            System.out.print("\nYour GPA is 0.66.\n");
-        } else if (letterGrade.equalsIgnoreCase("F")) {
-            System.out.print("\nYour GPA is 0.00.\n");
+        switch (letterGrade) {
+            case "A+":
+                System.out.println("\nYour GPA is 4.00.");
+            case "A":
+                System.out.println("\nYour GPA is 4.00.");
+                break;
+            case "A-":
+                System.out.println("\nYour GPA is 3.66.");
+            case "B+":
+                System.out.println("\nYour GPA is 3.33.");
+                break;
+            case "B":
+                System.out.println("\nYour GPA is 3.00.");
+                break;
+            case "B-":
+                System.out.println("\nYour GPA is 2.66.");
+                break;
+            case "C+":
+                System.out.println("\nYour GPA is 2.33.");
+                break;
+            case "C":
+                System.out.println("\nYour GPA is 2.00.");
+                break;
+            case "C-":
+                System.out.println("\nYour GPA is 1.66.");
+                break;
+            case "D+":
+                System.out.println("\nYour GPA is 1.33.");
+                break;
+            case "D":
+                System.out.println("\nYour GPA is 1.00.");
+                break;
+            case "D-":
+                System.out.println("\nYour GPA is 0.66.");
+                break;
+            case "F":
+                System.out.println("\nYour GPA is 0.00.");
+                break;
         }
     }
 
@@ -186,16 +195,81 @@ public class ProblemSet3 {
      *
      * Prompt the user to enter a playing card. What card was entered?
      */
-
     public void cards() {
-
         System.out.print("Enter a card: ");
-        String cardSuit = in.nextLine();
-           
+        String cardSuitAndRank = in.nextLine();
+        String cardSuit;
+        String cardRank = cardRank(cardSuitAndRank);
+        switch (cardSuitAndRank.substring(1, 2).toLowerCase()) {
+            case "c":
+                cardSuit = "Clubs";
+                break;
+            case "d":
+                cardSuit = "Diamonds";
+                break;
+            case "h":
+                cardSuit = "Hearts";
+                break;
+            case "s":
+                cardSuit = "Spades";
+                break;
+            default:
+                cardSuit = "false";
+        }
+        if(cardSuit == "false") {
+            System.out.print("That's not a valid suit.");
+        } else if(cardRank == "false") {
+            System.out.print("That's not a valid rank.");
+        } else {
+            System.out.printf("%s of %s.", cardRank, cardSuit);
+        }
     }
 
-    public void cardRank() {
-
+    //Returns the rank of the card given the character representing the rank
+    public String cardRank(String cardSuitAndRank) {
+        String cardRank;
+        switch (cardSuitAndRank.substring(0, 1).toLowerCase()) {
+            case "2":
+                cardRank = "Two";
+                return cardRank;
+            case "3":
+                cardRank = "Three";
+                return cardRank;
+            case "4":
+                cardRank = "Four";
+                return cardRank;
+            case "5":
+                cardRank = "Five";
+                return cardRank;
+            case "6":
+                cardRank = "Six";
+                return cardRank;
+            case "7":
+                cardRank = "Seven";
+                return cardRank;
+            case "8":
+                cardRank = "Eight";
+                return cardRank;
+            case "9":
+                cardRank = "Nine";
+                return cardRank;
+            case "t":
+                cardRank = "Ten";
+                return cardRank;
+            case "j":
+                cardRank = "Jack";
+                return cardRank;
+            case "q":
+                cardRank = "Queen";
+                return cardRank;
+            case "k":
+                cardRank = "King";
+                return cardRank;
+            case "a":
+                cardRank = "Ace";
+                return cardRank;
+        }
+        return "false";
     }
 
     /*
@@ -205,7 +279,17 @@ public class ProblemSet3 {
      */
 
     public void leapYear() {
-
+        System.out.print("Enter a year: ");
+        long year = in.nextLong();
+        if(year % 400 == 0) {
+            System.out.printf("\n%d is a leap year.", year);
+        } else if (year % 100 == 0) {
+            System.out.printf("\n%d is not a leap year.", year);
+        } else if (year % 4 == 0) {
+            System.out.printf("\n%d is a leap year.", year);
+        } else {
+            System.out.printf("\n%d is not a leap year.", year);
+        }
     }
 
     /*
@@ -216,7 +300,11 @@ public class ProblemSet3 {
      */
 
     public void state() {
-
+        System.out.print("Enter a temperature: ");
+        double temperature = in.nextDouble();
+        System.out.print("Enter a scale: ");
+        char scale = in.next().charAt(0);
+        
     }
 
     /*
